6bc918c04b592a97a58edba024b4a604bc2f7cd1
Tuna Ayberk Ozmen <tunaayberk@hotmail.com>
2019-03-14 01:56:42 -0400

    First Commit

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,9 +1,4 @@
 export const DB_CONFIG = {
-  apiKey: "AIzaSyDsgYvlMg3qdrhoB6hjR0UxcbMv1dVuZt8",
-  authDomain: "react-to-do-55919.firebaseapp.com",
-  databaseURL: "https://react-to-do-55919.firebaseio.com",
-  projectId: "react-to-do-55919",
-  storageBucket: "react-to-do-55919.appspot.com",
-  messagingSenderId: "964524971610"
+  /*DELETED API CONFIG */
 };
   
